b53b3289f77d778e8aa0bcbe8645f44cca9dab9c
wrli <wrli.vebmaster@gmail.com>
2012-06-01 21:56:18 +0200

    init

---
+++ src/atv/ATV.java
@@ -0,0 +1,17 @@
+package atv;
+
+import processing.core.PApplet;
+
+
+public class ATV extends PApplet {
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { atv.ATV.class.getName() });
+	}
+}
